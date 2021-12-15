0c828cb163fa9217a3ca0fa57e35df7858ba9abd
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-12-16 05:29:00 +0700

    2. data types
    (updated) add the error explanation when the compiler runnig the solidity file

--- Contracts/2-DataTypes.sol
+++ Contracts/2-DataTypes.sol
@@ -10,6 +10,11 @@ contract ValueTypes {
     //ParseEror: Expected ';' but got 'int' -> because the compiler read straighforward to the next line of solidityh syntax if you don't placed semicolon to end your argument/declaration
     bool publi b = true
     int public i = -122;
+
+     # it will cause an error "Failed Compile -> ParserError: Function, variable, struct or modifier declaration expected."
+    public int i = 123; -> wrong typing, so the solidity compiler can't read your code
+    // public address addr = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    // public bytes32 b32 = 0x89c58ced8a9078bdef2bb60f22e58eeff7dbfed6c2dff3e7c508b629295926fa;
     */
     bool public b = true;
     int public i = -122;
